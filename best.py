fdb7d0230570585f678bf2d6181acc80a345f655
hao01-art <hao01@graduate.utm.my>
2025-12-02 11:04:45 +0800

    Update best.py

--- best.py
+++ best.py
@@ -11,7 +11,7 @@ import time
 import os
 
 # ====================== USER SETTINGS ======================
-MODEL_PATH = "/home/ben/yolo/psm_bottle_yolov8s_model2_ncnn"
+MODEL_PATH = "/home/ben/yolo/psm_bottle_yolov8s_model2.pt"
 CONF_THRESHOLD = 0.5
 IOU_THRESHOLD = 0.6
 FONT = cv2.FONT_HERSHEY_SIMPLEX
@@ -141,5 +141,6 @@ picam2.stop()
 cv2.destroyAllWindows()
 
 print("✔ Resources released. Goodbye!")
+
 
 
